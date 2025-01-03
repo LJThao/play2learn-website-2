f9bbe8367c0302f031df0338ae3b71f1b2ba9982
LJThao <yengfong.thao@atlasschool.com>
2025-01-02 18:55:53 -0600

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
 
+  // Set the publicPath for GitHub Pages
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/play2learn-website-2/'
+  : '/'
+})
