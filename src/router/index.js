d58ff3b83a21e179f5f62b5a51abc6ff98fb9a41
LJThao <yengfong.thao@atlasschool.com>
2025-01-03 15:19:46 -0600

    removing hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../components/HomePage.vue';
 import AnagramHunt from '../components/AnagramHunt.vue';
 
@@ -8,7 +8,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),  // Switch to hash history mode
+  history: createWebHistory(), // Switch to history mode (no hash in URL)
   routes,
 });
 
