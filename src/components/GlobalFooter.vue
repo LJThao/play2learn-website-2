52ddb262709246f46a83c50da0ff5bad0c48cec2
LJThao <yengfong.thao@atlasschool.com>
2025-01-02 20:03:29 -0600

    update paths

--- src/components/GlobalFooter.vue
+++ src/components/GlobalFooter.vue
@@ -5,9 +5,9 @@
       <p class="mb-0">&copy; {{new Date().getFullYear()}} Play2Learn</p>
       <div class="d-flex justify-content-end align-items-center">
         <a href="/play2learn-website-2/contact-us.html" class="mx-2"><img src="/play2learn-website-2/images/icon-contact-us.png" alt="Contact Us"></a>
-        <a href="https://instagram.com" class="mx-2"><img src="play2learn-website-2/images/icon-instagram.png" alt="Instagram"></a>
-        <a href="https://twitter.com" class="mx-2"><img src="play2learn-website-2/images/icon-twitter.png" alt="Twitter"></a>
-        <a href="https://facebook.com" class="mx-2"><img src="play2learn-website-2/images/icon-facebook.png" alt="Facebook"></a>
+        <a href="https://instagram.com" class="mx-2"><img src="/play2learn-website-2/images/icon-instagram.png" alt="Instagram"></a>
+        <a href="https://twitter.com" class="mx-2"><img src="/play2learn-website-2/images/icon-twitter.png" alt="Twitter"></a>
+        <a href="https://facebook.com" class="mx-2"><img src="/play2learn-website-2/images/icon-facebook.png" alt="Facebook"></a>
       </div>
     </div>
 </footer>
